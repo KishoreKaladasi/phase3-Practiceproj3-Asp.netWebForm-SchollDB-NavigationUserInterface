e2ca0d6bc371fb838b5efb8dbc9e38a6274afe00
KishoreKaladasi <kishorekaladasi2001@gmail.com>
2023-11-29 21:48:46 +0530

    first commit

---
+++ ManageSchoolDatabase-ThroughWebForms-ASP.NET/Student.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ManageSchoolDatabase_ThroughWebForms_ASP.NET
+{
+    public partial class Student : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGridView();
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Handle selection change if needed
+        }
+
+        private void BindGridView()
+        {
+            using (SqlConnection cn = new SqlConnection("Data Source=KISHORE\\SQLEXPRESS;Initial Catalog=School;Integrated Security=True"))
+            {
+                cn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT * FROM Students", cn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                GridView1.DataSource = reader;
+                GridView1.DataBind();
+
+                reader.Close();
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Subjects.aspx");
+        }
+    }
+}
